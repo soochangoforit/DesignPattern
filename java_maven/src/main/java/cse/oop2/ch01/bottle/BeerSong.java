9c4ad3410c0131a9042d41d4c62402793ada4786
soochangoforit <tncksdl05@gmail.com>
2022-03-21 13:50:09 +0900

    first test commit

--- java_maven/src/main/java/cse/oop2/ch01/bottle/BeerSong.java
+++ java_maven/src/main/java/cse/oop2/ch01/bottle/BeerSong.java
@@ -21,7 +21,7 @@ public class BeerSong {
            System.out.println(beerNum +" " + word + " of beer on the wall");
            System.out.println(beerNum + " " + word + " of beer.");
            System.out.println("Take one down.");
-           System.out.println("Pass it around");
+           System.out.println("Pass it around" + "SSS");
            beerNum = beerNum - 1;
            
            if(beerNum == 1) {
