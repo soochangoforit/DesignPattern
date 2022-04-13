6653698b96122960e304cda430683da8c43305a9
soochangoforit <tncksdl05@gmail.com>
2022-04-13 21:29:10 +0900

    add factory pattern

---
+++ DesignPattern/src/main/java/cse/oop2/designpattern/ch04/CheesePizza.java
@@ -0,0 +1,15 @@
+
+package cse.oop2.designpattern.ch04;
+
+
+public class CheesePizza extends Pizza {
+    
+    public CheesePizza(){
+        name = "Cheese Pizza";
+        dough = "Regular Crust";
+        sauce = "Marinara Pizza Sauce";
+        toppings.add("Fresh Mozzarella");
+        toppings.add("Parmesan");
+    }
+    
+}
